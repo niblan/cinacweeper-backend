8d366423d13c411eae29f6cc42e807266016069c
Artur Pelcharskyi <pelcharskyi.pn@ucu.edu.ua>
2023-03-30 19:12:09 +0300

    move documentation

--- src/cinasweeper_backend/cinasweeper_logic/move.py
+++ src/cinasweeper_backend/cinasweeper_logic/move.py
@@ -6,8 +6,15 @@ from dataclasses import dataclass
 
 @dataclass
 class Move:
-    """A single move in a game of Cinasweeper."""
+    """Represents a single move in a game of Cinasweeper.
 
+    Attributes:
+        x (int): The x-coordinate of the move.
+        y (int): The y-coordinate of the move.
+        action (int): The action taken for the move, where:
+                        0 - Flag cell
+                        1 - Reveal cell
+    """
     x: int
     y: int
     action: int
